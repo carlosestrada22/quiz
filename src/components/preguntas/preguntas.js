c0d136b0618c833bdfab3a4812b58724b5f76f40
cestradaTesselar <cestrada@tesselar.mx>
2017-12-15 10:08:43 -0600

    style Eliminada info demo

--- src/components/preguntas/preguntas.js
+++ src/components/preguntas/preguntas.js
@@ -2,41 +2,6 @@ import React, { Component } from 'react';
 import Pregunta from '../pregunta/pregunta.js'
 import './preguntas.css'
 
-let Questions = [
-    {
-        Id: 0,
-        Pregunta: "Para ti ¿Qué tan importante es la palabra?",
-        Palabra: "Quebradita"
-    },
-    {
-        Id: 1,
-        Pregunta: "Para ti ¿Qué tan fiable es la palabra?",
-        Palabra: "Quesadilla"
-    },
-    {
-        Id: 2,
-        Pregunta: "Para ti ¿Qué tan mala es la palabra?",
-        Palabra: "Mouse"
-    },
-    {
-        Id: 3,
-        Pregunta: "Para ti ¿Qué tan buena es la palabra?",
-        Palabra: "Bocina"
-    },
-    {
-        Id: 4,
-        Pregunta: "Para ti ¿Qué tan terrible es la palabra?",
-        Palabra: "Sandía"
-    },
-    {
-        Id: 5,
-        Pregunta: "Para ti ¿Qué tan amenazante es la palabra?",
-        Palabra: "Taco"
-    },
-]
-let ActualPregunta = Questions[0]
-let Index = 0
-
 class Preguntas extends Component {
     constructor(props) {
         super(props)
@@ -51,29 +16,17 @@ class Preguntas extends Component {
     componentDidMount() {
         this.props.axios.get(`http://${window.location.hostname}:3008/questions`)
             .then(res => {
-                // console.log(res)
                 this.setState({Questions: res.data})
-
-                console.log(this.state.Questions)
+                // console.log(this.state.Questions)
             })
     }
     render() {
         return (
             <div id="preguntas" className="preguntas">
-                <Pregunta Preguntas={this.state.Questions} Avanzar={() => Avanzar(this)} $={this.props.$} cambiarVista={this.props.cambiarVista} />
+                <Pregunta Preguntas={this.state.Questions} $={this.props.$} cambiarVista={this.props.cambiarVista} />
             </div>
         )
     }
-}
-
-const Avanzar = (ref) => {
-    // console.log(Index, Questions.length)
-    if (Index < ref.state.Questions.length - 1)
-        Index++
-
-    // this.setState({Actual : Questions[Index]})
-    ActualPregunta = ref.state.Questions[Index]
-    // console.log("AVANZANDO ALV", ActualPregunta)
 }
 
 export default Preguntas
